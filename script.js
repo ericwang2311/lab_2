aa8c3d4cd4cbcc7a901ed9da3af5bb5f59981593
ericwang2311 <lightningleonhart@gmail.com>
2020-09-10 23:18:45 -0400

    Attractions

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // TODO: load the dataset
+let attractions;
+fetch("attractions.json")
+  .then((response) => response.json())
+  .then((data) => {
+    attractions = data;
+    console.log("attractions - (a)", attractions);
+  });
+console.log("attractions - (b)", attractions);
 
 function filterData(category) {
   /* **************************************************
    *
    * TODO: filter attractions by the selected category
-   * TODO: filter top 5 attractions
-   *
+   */
+  //TODO: filter top 5 attractions
+
+
+  
+  /*
    * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
    *
    * renderBarChart(data)
