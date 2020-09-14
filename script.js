6381edd8b85f7dad90152be3b67696f0b4ed9266
ericwang2311 <lightningleonhart@gmail.com>
2020-09-14 15:29:46 -0400

    Finished

--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 // TODO: load the dataset
 let attractions;
+
+let filteredAttractions;
+
+let topFiveAttractions;
+
+let selectElement;
+
 fetch("attractions.json")
   .then((response) => response.json())
   .then((data) => {
     attractions = data;
-    console.log("attractions - (a)", attractions);
+    console.log(attractions);
+    filterData("all");
   });
-console.log("attractions - (b)", attractions);
 
 function filterData(category) {
-  /* **************************************************
-   *
-   * TODO: filter attractions by the selected category
-   */
+  //TODO: filter attractions by the selected category
   //TODO: filter top 5 attractions
+  console.log(category);
+  filteredAttractions = attractions;
 
+  if (category != "all") {
+    filteredAttractions = attractions.filter(
+      (test) => test.Category == category
+    );
+    console.log(filteredAttractions);
+  }
 
-  
-  /*
-   * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-   *
-   * renderBarChart(data)
-   *
-   * - 'data' must be an array of JSON objects
-   * - the max. length of 'data' is 5
-   *
-   * **************************************************/
+  filteredAttractions.sort(function (a, b) {
+    return b.Visitors - a.Visitors;
+  });
+  console.log(filteredAttractions);
+
+  topFiveAttractions = filteredAttractions.slice(0, 5);
+  console.log(topFiveAttractions);
+  renderBarChart(topFiveAttractions);
 }
 
-// TODO: Define an event listener for the dropdown menu
-//       Call filterData with the selected category
+/*
+  CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
+  // renderBarChart(data)
+
+
+ TODO: Define an event listener for the dropdown menu
+ Call filterData with the selected category 
+ */
+
+selectElement = document.querySelector("#attraction-category");
+
+selectElement.addEventListener("change", () => {
+  filterData(selectElement.value);
+});
